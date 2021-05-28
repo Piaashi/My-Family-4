cbf2861ecc95ad84c6fb5f542afc32f28a49b710
Piaashi <75792228+Piaashi@users.noreply.github.com>
2021-05-28 20:07:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+Webcam.set({
+
+    width:350,height:300,image_format:"png",png_quality:90
+    
+    });
+    
+    camera=document.getElementById("camera");
+    Webcam.attach("#camera");
+    function capture_image() 
+    {
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    
+        });
+    }
+    
+    console.log("ml5version: ",ml5.version);
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9ZUa255V4/model.json",modelLoaded);
+    
+    function modelLoaded()
+    {
+        console.log("Model is loaded");
+    }
+
+
+    function identify_image() 
+{
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results) 
+{
+  if (error) 
+  {
+      console.error(error);
+  }   
+  
+  else 
+   {
+       console.log(results); 
+       document.getElementById("result_object_name").innerHTML=results[0].label;
+       document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(5);
+   }
+   
+}
